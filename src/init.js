7d2e53709980ded314af50a917e2088d5caa2c71
Sara Brandes <smbrandes17@gmail.com>
2018-01-23 20:17:37 -0800

    adjusted sizes for laptop

--- src/init.js
+++ src/init.js
@@ -2,35 +2,35 @@ $(document).ready(function() {
   window.dancers = [];
   window.dogs = [];
   window.cat = [];
-  
-  var tennisBall = new Ball(700, 1150);
+
+  var tennisBall = new Ball(575, 1100);
   $('body').append(tennisBall.$node);
-  
-  
-  
+
+
+
   setInterval (function () {
     if (window.cat.length) {
       window.dogs.forEach (function (dog) {
         dog.chaseCat();
       });
     }
-    
+
     window.dogs.forEach( function (dog) {
       var cat = $('#cat');
-      
+
       var dogLeft = Math.round(dog.$node.css('left').split('px')[0]);
       var dogTop = Math.round(dog.$node.css('top').split('px')[0]);
-      
+
       var catLeft = Number($('#cat').css('left').split('px')[0]);
       var catTop = Number($('#cat').css('top').split('px')[0]);
-      
+
       if (dogLeft === catLeft && dogTop === catTop) {
         $('#cat').remove();
         var remove = window.cat.pop();
         delete window.cat.pop();
-        
-        alert('Oh no I am Cat I have been ate!!!');
-        
+
+        alert('OMG UR DOG ATE MAAH KITTEHHHH!!!');
+
         setTimeout (function () {
           window.dogs.forEach(function (dog) {
             dog.stopChasing();
@@ -39,22 +39,22 @@ $(document).ready(function() {
       }
     });
   }, 200);
-  
-  
+
+
 
   $('.addDancerButton').on('click', function(event) {
     event.preventDefault();
 
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-    
+
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
     var dancer = new dancerMakerFunction(
-      650 * Math.random() + 15,
-      1400 * Math.random(),
+      600 * Math.random() + 15,
+      1100 * Math.random(),
       Math.random() * 1000
     );
-    
+
 
     if (dancerMakerFunctionName === 'Cat' && window.cat.length === 0) {
       $(document).mousemove( function(e) {
@@ -64,37 +64,36 @@ $(document).ready(function() {
 
       window.cat.push(dancer);
       $('body').append(dancer.$node);
-      
+
     } else if (dancerMakerFunctionName !== 'Cat') {
       window.dogs.push(dancer);
       $('body').append(dancer.$node);
     }
   });
-  
+
   var throwTheBall = function (event) {
     event.preventDefault();
     tennisBall.throw();
-  
+
     setTimeout( function () {
       window.dogs.forEach( function (dog) {
         dog.chaseBall();
       });
-      
-      tennisBall = new Ball (700, 1150);
+
+      tennisBall = new Ball (575, 1100);
       $('body').append(tennisBall.$node);
     }, 300);
-    
+
     setTimeout( function () {
       window.dogs.forEach( function (dog) {
         dog.stopChasing();
       });
     }, 1500);
   };
-  
-  $('.ballThrow').on('click', throwTheBall);
-  
-  $('.ball').on('click', throwTheBall);
-  
-  
-});
 
+  $('.ballThrow').on('click', throwTheBall);
+
+  $('.ball').on('click', throwTheBall);
+
+
+});
