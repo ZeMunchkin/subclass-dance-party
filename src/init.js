2621213f2b852cfe6883eed5dfb493adecca4e6e
Sara Brandes <smbrandes17@gmail.com>
2018-01-26 09:41:40 -0800

    modifications for smaller laptop screen

--- src/init.js
+++ src/init.js
@@ -1,99 +1,63 @@
 $(document).ready(function() {
   window.dancers = [];
-  window.dogs = [];
-  window.cat = [];
-
-  var tennisBall = new Ball(575, 1100);
-  $('body').append(tennisBall.$node);
-
-
-
-  setInterval (function () {
-    if (window.cat.length) {
-      window.dogs.forEach (function (dog) {
-        dog.chaseCat();
-      });
-    }
-
-    window.dogs.forEach( function (dog) {
-      var cat = $('#cat');
-
-      var dogLeft = Math.round(dog.$node.css('left').split('px')[0]);
-      var dogTop = Math.round(dog.$node.css('top').split('px')[0]);
-
-      var catLeft = Number($('#cat').css('left').split('px')[0]);
-      var catTop = Number($('#cat').css('top').split('px')[0]);
-
-      if (dogLeft === catLeft && dogTop === catTop) {
-        $('#cat').remove();
-        var remove = window.cat.pop();
-        delete window.cat.pop();
-
-        alert('OMG UR DOG ATE MAAH KITTEHHHH!!!');
-
-        setTimeout (function () {
-          window.dogs.forEach(function (dog) {
-            dog.stopChasing();
-          });
-        }, 500);
-      }
-    });
-  }, 200);
-
-
+  var catInThePark = false;
 
   $('.addDancerButton').on('click', function(event) {
     event.preventDefault();
+    /* This function sets up the click handlers for the create-dancer
+     * buttons on dancefloor.html. You should only need to make one small change to it.
+     * As long as the "data-dancer-maker-function-name" attribute of a
+     * class="addDancerButton" DOM node matches one of the names of the
+     * maker functions available in the global scope, clicking that node
+     * will call the function to make the dancer.
+     */
 
+    /* dancerMakerFunctionName is a string which must match
+     * one of the dancer maker functions available in global scope.
+     * A new object of the given type will be created and added
+     * to the stage.
+     */
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+    
 
+
+    // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
 
+    // make a dancer with a random position
+    
+    
+
     var dancer = new dancerMakerFunction(
-      600 * Math.random() + 15,
-      1100 * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
+    
 
 
-    if (dancerMakerFunctionName === 'Cat' && window.cat.length === 0) {
+    if(dancerMakerFunctionName === 'Cat' && catInThePark === false) {
+      catInThePark = true;
       $(document).mousemove( function(e) {
         $('#cat').stop().animate({left: e.pageX, top: e.pageY}, 300);
+        $('body').css({'cursor': 'url(http://bit.ly/1rhj0Mp), auto'});
       });
-
-
-      window.cat.push(dancer);
+      
+      window.dancers.push(dancer);
       $('body').append(dancer.$node);
-
+      
     } else if (dancerMakerFunctionName !== 'Cat') {
-      window.dogs.push(dancer);
+      window.dancers.push(dancer);
       $('body').append(dancer.$node);
     }
+    
+    
+
+    
+
+    
+    console.log(window.dancers);
+    
   });
-
-  var throwTheBall = function (event) {
-    event.preventDefault();
-    tennisBall.throw();
-
-    setTimeout( function () {
-      window.dogs.forEach( function (dog) {
-        dog.chaseBall();
-      });
-
-      tennisBall = new Ball (575, 1100);
-      $('body').append(tennisBall.$node);
-    }, 300);
-
-    setTimeout( function () {
-      window.dogs.forEach( function (dog) {
-        dog.stopChasing();
-      });
-    }, 1500);
-  };
-
-  $('.ballThrow').on('click', throwTheBall);
-
-  $('.ball').on('click', throwTheBall);
-
-
 });
+
